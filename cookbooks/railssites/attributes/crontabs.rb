ca62c1fa579809bf008032a6edb2ee6f5985aac8
Jacobo Garcia <jacobo@Electric-Sweat.local>
2009-12-29 20:05:10 +0100

    Fix: setting properly attributes which is done with set

--- cookbooks/railssites/attributes/crontabs.rb
+++ cookbooks/railssites/attributes/crontabs.rb
@@ -1,10 +1,10 @@
-# default[:crontabs][:mailto]="ballsbreaking@bebanjo.net"
-# default[:crontabs][:path]="/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin"
-# default[:crontabs][:shell]="/bin/sh"
+# set[:crontabs][:mailto]="ballsbreaking@bebanjo.net"
+# set[:crontabs][:path]="/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin"
+# set[:crontabs][:shell]="/bin/sh"
 
-default[:crontabs][:sequence][:ts] = { :minute => "@reboot", :hour => "", :day => "", :month => "", :weekday => "",
+set[:crontabs][:sequence][:ts] = { :minute => "@reboot", :hour => "", :day => "", :month => "", :weekday => "",
   :command => "cd /var/www/apps/tsa/current && RAILS_ENV=production rails ts:in ts:start" }
-default[:crontabs][:sequence][:delayed_job] = { :minute => "@reboot", :hour => "", :day => "", :month => "", :weekday => "",
+set[:crontabs][:sequence][:delayed_job] = { :minute => "@reboot", :hour => "", :day => "", :month => "", :weekday => "",
   :command => "cd /var/www/apps/tsa/current && RAILS_ENV=production script/job_runner start 0; RAILS_ENV=production script/job_runner start 1" }
 
 
