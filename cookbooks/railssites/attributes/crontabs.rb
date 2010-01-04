cbccff3ae579fdf003c1cd15742c84c3ee6ed9fb
Luismi Cavalle <luismi@lmcavalle.com>
2010-01-04 17:53:22 +0100

    Fix: Set correct crontabs for sequence

--- cookbooks/railssites/attributes/crontabs.rb
+++ cookbooks/railssites/attributes/crontabs.rb
@@ -2,10 +2,15 @@
 # set[:crontabs][:path]="/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin"
 # set[:crontabs][:shell]="/bin/sh"
 
-set[:crontabs][:sequence][:ts] = { :minute => "@reboot", :hour => "", :day => "", :month => "", :weekday => "",
-  :command => "cd /var/www/apps/tsa/current && RAILS_ENV=production rails ts:in ts:start" }
-set[:crontabs][:sequence][:delayed_job] = { :minute => "@reboot", :hour => "", :day => "", :month => "", :weekday => "",
-  :command => "cd /var/www/apps/tsa/current && RAILS_ENV=production script/job_runner start 0; RAILS_ENV=production script/job_runner start 1" }
+# set[:crontabs][:sequence][:ts] = { 
+#   :minute => "@reboot", :hour => "", :day => "", :month => "", :weekday => "", 
+#   :command => "cd /var/www/apps/tsa/current && RAILS_ENV=production rails ts:in ts:start" 
+# }
+# 
+# set[:crontabs][:sequence][:delayed_job] = { 
+#   :minute => "@reboot", :hour => "", :day => "", :month => "", :weekday => "",
+#   :command => "cd /var/www/apps/tsa/current && RAILS_ENV=production script/job_runner start 0; RAILS_ENV=production script/job_runner start 1" 
+# }
 
 
 
