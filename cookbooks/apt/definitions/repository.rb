2aab66a638da357896aa13a2914ee247025f981c
Jacobo Garcia <jacobo@Electric-Sweat.local>
2010-01-21 13:02:30 +0100

    Fix: templates are not really required here

--- cookbooks/apt/definitions/repository.rb
+++ cookbooks/apt/definitions/repository.rb
@@ -7,23 +7,13 @@ define :repository do
     mode "0755"
   end
 
-  if params[:config_url] != nil
-    remote_file "/etc/apt/sources.list.d/#{params[:name]}.list" do
-      source params[:config_url]
-      owner "root"
-      group "root"
-      mode "0644"
-      action :create
-    end
-  else
-    template "/etc/apt/sources.list.d/#{params[:name]}.list" do
-      source "repository.erb"
-      owner "root"
-      group "root"
-      mode "0644"
-      action :create
-      variables :repo_line => params[:repo_line]
-    end
+
+  remote_file "/etc/apt/sources.list.d/#{params[:name]}.list" do
+    params[:config_url] ? source params[:config_url] : source params[:name]
+    owner "root"
+    group "root"
+    mode "0644"
+    action :create
   end
 
   execute "add-key" do
