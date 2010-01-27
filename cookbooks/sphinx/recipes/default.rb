436a14f3793800de0737ad946120b3bbc11dc3a3
Jacobo Garcia <jacobo@Electric-Sweat.local>
2010-01-27 18:07:59 +0100

    Fix: adding file checksums so we don't re-download packages every time. Fix: improving the way we test that sphinx is installed so we don't re-install sphinx every chef run'

--- cookbooks/sphinx/recipes/default.rb
+++ cookbooks/sphinx/recipes/default.rb
@@ -28,17 +28,20 @@ if not File.exists?(node[:sphinx][:path])
   remote_file node[:sphinx][:tar_file] do
     source node[:sphinx][:url]
     mode "0755"
+    checksum node[:sphinx][:tar_file_checksum]
   end
 
   remote_file "#{node[:sphinx][:src_path]}/libstemmer_c.tgz" do
     source "http://snowball.tartarus.org/dist/libstemmer_c.tgz"
     mode "0755"
+    checksum "b642b8921915128bd95c99d302d33701cba8b5afbda81b762c118cbadfb7670f"
   end
 
   script "install_sphinx" do
     interpreter "bash"
     user "root"
     cwd node[:sphinx][:src_path]
+    not_if "test -f /usr/local/bin/searchd"
     code <<-EOH
     tar -xvf #{node[:sphinx][:tar_file]}
     cd  #{node[:sphinx][:version]}
@@ -47,5 +50,5 @@ if not File.exists?(node[:sphinx][:path])
     make
     make install
     EOH
-    end
+   end
 end
