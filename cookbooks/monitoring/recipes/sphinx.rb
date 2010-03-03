582299c399645e96acc9d5640183d55956e8c2d1
Jacobo Garcia <jacobo@Electric-Sweat.local>
2010-03-03 22:48:22 +0100

    Feat: monit sphinx recipe

---
+++ cookbooks/monitoring/recipes/sphinx.rb
@@ -0,0 +1,12 @@
+include_recipe 'monitoring'
+
+node[:monit][:sphinx].each do |appname|
+  template "/etc/monit/conf.d/sphinx-#{appname}" do
+    source "sphinx.erb"
+    owner "root"
+    group "root"
+    mode 0644
+    variables ( :appname => appname )
+    notifies :restart, resources(:service => "monit")
+  end
+end
