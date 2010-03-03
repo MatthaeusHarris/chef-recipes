1487d4fe3b5c989805c8a9aabbddaf04df10f3ac
Jacobo Garcia <jacobo@Electric-Sweat.local>
2010-03-03 22:38:04 +0100

    Setting copyright

--- cookbooks/railssites/recipes/default.rb
+++ cookbooks/railssites/recipes/default.rb
@@ -1,8 +1,9 @@
+# -*- coding: utf-8 -*-
 #
 # Cookbook Name:: railssites
 # Recipe:: default
 #
-# Copyright 2009, Example Com
+# Copyright 2009, Jacobo García López de Araujo
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
