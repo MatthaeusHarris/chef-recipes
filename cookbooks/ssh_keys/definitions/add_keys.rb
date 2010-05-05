1fac2552c250c0ac869a9c29e62b90f6a3a2536a
Jacobo García <therobot@aspgems.com>
2010-05-05 17:05:40 +0200

    Feat: adding support for adding several ssh public keys to a app user account. It uses a databag for storing keys

--- cookbooks/ssh_keys/definitions/add_keys.rb
+++ cookbooks/ssh_keys/definitions/add_keys.rb
@@ -1,16 +1,28 @@
 define :add_keys do
-  
+
   config = params[:conf]
   name = params[:name]
   keys = Mash.new
-  keys[name] = node[:ssh_keys][name]
-    
+
+  if config[:group].to_s.eql?("admin")
+    keys[name] = node[:ssh_keys][name]
+  else
+    users=data_bag('keys_catalog')
+    users_keys=[]
+    users.each do |user|
+      user_data=data_bag_item('keys_catalog',user)
+      users_keys << user_data["ssh_key"]
+    end
+    keys[name] = users_keys << node[:ssh_keys][name]
+    keys[name].flatten!.uniq!
+  end
+
   template "/home/#{name}/.ssh/authorized_keys" do
     source "authorized_keys.erb"
     action :create
     cookbook "ssh_keys"
     owner name
-    group config[:group].to_s
+    group (config[:group]).to_s
     variables(:keys => keys)
     mode 0600
   end
