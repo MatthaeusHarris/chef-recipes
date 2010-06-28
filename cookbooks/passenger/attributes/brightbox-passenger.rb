9c46f928a8923d8b04fb5468cd88fa553cca6397
Jacobo García <therobot@aspgems.com>
2010-06-28 17:10:30 +0200

    Fix: upgrading to latest passenger version

--- cookbooks/passenger/attributes/brightbox-passenger.rb
+++ cookbooks/passenger/attributes/brightbox-passenger.rb
@@ -1,7 +1,7 @@
-set_unless[:passenger][:version]="2.2.7"
-set_unless[:passenger][:passengermaxrequests]="128"
-set_unless[:passenger][:brightbox][:passengerroot]="/usr/"
-set_unless[:passenger][:brightbox][:passengerruby]="/usr/bin/ruby"
-set_unless[:passenger][:brightbox][:gem_path] = "/usr/bin/gem"
-set_unless[:passenger][:brightbox][:install_binary]=nil
-set_unless[:passenger][:brightbox][:module] = "/usr/lib/apache2/modules/mod_passenger.so"
+default[:passenger][:version]="2.2.14"
+default[:passenger][:passengermaxrequests]="128"
+default[:passenger][:brightbox][:passengerroot]="/usr/"
+default[:passenger][:brightbox][:passengerruby]="/usr/bin/ruby"
+default[:passenger][:brightbox][:gem_path] = "/usr/bin/gem"
+default[:passenger][:brightbox][:install_binary]=nil
+default[:passenger][:brightbox][:module] = "/usr/lib/apache2/modules/mod_passenger.so"
