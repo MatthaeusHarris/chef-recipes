fc4316890d3b7c0f01cf1979cdfb3ad075f3cd2e
Jacobo García <therobot@aspgems.com>
2010-07-01 18:04:53 +0200

    Upgrading to latest passenger

--- cookbooks/passenger/attributes/default.rb
+++ cookbooks/passenger/attributes/default.rb
@@ -1,18 +1,18 @@
-set_unless[:passenger][:version]="2.2.7"
-set_unless[:passenger][:passengermaxrequests]="128"
+default[:passenger][:version]="2.2.14"
+default[:passenger][:passengermaxrequests]="128"
 
 mem=(memory[:total].gsub("kB","")).to_i
 case
 when (mem <= 329036)
-  set_unless[:passenger][:passengermaxpoolsize]="2"
+  default[:passenger][:passengermaxpoolsize]="2"
 when (mem <= 524508)
-  set_unless[:passenger][:passengermaxpoolsize]="6"
+  default[:passenger][:passengermaxpoolsize]="6"
 when (mem <= 1048796)
-  set_unless[:passenger][:passengermaxpoolsize]="14"
+  default[:passenger][:passengermaxpoolsize]="14"
 when (mem <= 2097372)
-  set_unless[:passenger][:passengermaxpoolsize]="30"
+  default[:passenger][:passengermaxpoolsize]="30"
 else
-  set_unless[:passenger][:passengermaxpoolsize]="62"
+  default[:passenger][:passengermaxpoolsize]="62"
 end
 
 
