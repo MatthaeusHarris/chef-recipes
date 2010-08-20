1eb0544208568384002aa98f24eee4cbccd23eae
Jacobo García <therobot@aspgems.com>
2010-08-20 20:51:46 +0200

    Feat: adding support for ubuntu 10.04

--- cookbooks/gems/attributes/default.rb
+++ cookbooks/gems/attributes/default.rb
@@ -4,4 +4,13 @@ else
   set[:gems][:binary]="/usr/bin/gem"
 end
 
-default[:gems][:packages]=%w( mysql bundler newrelic_rpm )
+case platform_version
+when "8.04"
+  dependencies=%w(libmagick9-dev libmysqlclient15-dev)
+when "10.04"
+  dependencies=%w(libmagickcore-dev libmysqlclient-dev)
+end
+
+default[:gems][:dependencies]=%w(libxslt1-dev libxml2 libxml2-dev imagemagick libfreeimage-dev) + dependencies
+default[:gems][:packages]=%w(mysql bundler newrelic_rpm)
+
