1c1781d48567c3096bca984ff718cc502f8b9e4c
Jacobo García <therobot@aspgems.com>
2010-08-20 20:51:20 +0200

    Feat: adding packages as an attribute

--- cookbooks/gems/recipes/gem_dependencies.rb
+++ cookbooks/gems/recipes/gem_dependencies.rb
@@ -1,6 +1,4 @@
-packages = %w{ libxslt1-dev libxml2 libxml2-dev imagemagick libfreeimage-dev libmagick9-dev libmysqlclient15-dev }
-
-packages.each do |p|
+node[:gems][:dependencies].each do |p|
   package p do
     action :install
   end
