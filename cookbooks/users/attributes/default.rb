4e80fcde2a7572bcbb4a88229422584db89ad81f
Jacobo García <therobot@aspgems.com>
2010-08-21 04:21:43 +0200

    Feat: commenting the attributes

--- cookbooks/users/attributes/default.rb
+++ cookbooks/users/attributes/default.rb
@@ -1,10 +1,8 @@
-# -*- coding: utf-8 -*-
-
-
 # passwords must be in shadow password format with a salt. To generate: openssl passwd -1
 # you have to specify keys as an array
-
-set[:users][:frodo] = {:password => "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX", :comment => "Frodo Baggins", :group  => :admin}
-set[:ssh_keys][:frodo]  = [ "ssh-rsa XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX" ]
+#
+# Example:
+# set[:users][:frodo] = {:password => "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX", :comment => "Frodo Baggins", :group  => :admin}
+# set[:ssh_keys][:frodo]  = [ "ssh-rsa XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX" ]
 
 
