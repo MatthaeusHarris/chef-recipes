3ed73fa93eba9e4295e23d1ff7120f0b5f908e49
Jacobo García <therobot@aspgems.com>
2010-08-26 17:09:48 +0200

    Fix: loading fuse module load on boot

--- cookbooks/glusterfs/recipes/client.rb
+++ cookbooks/glusterfs/recipes/client.rb
@@ -14,6 +14,14 @@ directory node[:glusterfs][:client][:mount_directory] do
   recursive true
 end
 
+execute "add-fuse-to-modules" do
+  command "echo fuse >> /etc/modules"
+  user "root"
+  creates "/var/tmp/.add-fuse-to-modules"
+end
+
+file "/var/tmp/.add-fuse-to-modules"
+
 mount node[:glusterfs][:client][:mount_directory] do
   device "/etc/glusterfs/glusterfs.vol"
   fstype "glusterfs"
@@ -22,6 +30,3 @@ mount node[:glusterfs][:client][:mount_directory] do
   dump 0
   pass 0
 end
-
-
-
